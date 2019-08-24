b15492cb955d6c4df6ee6edf588ba0c5a665263c
Kuivalainen <Kuyvalaynen@gmail.com>
2019-08-24 12:55:45 +0300

    buttons and weather

--- src/App.js
+++ src/App.js
@@ -2,18 +2,64 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <WeatherDisplay zip={"12345"} />
+const PLACES = [
+  { name: "Moscow", zip: "Moscow,RU" },
+  { name: "Helsinki", zip: "helsinki,fi" },
+  { name: "Mikkeli", zip: "mikkeli,fi" },
+  { name: "Oulu", zip: "oulu,fi" }
+];
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0,
+    };
+  }
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div className="App">
+        {PLACES.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+            }}
+          >
+              {place.name}
+          </button>
+        ))}
+        <WeatherDisplay
+          key={activePlace}
+          zip={PLACES[activePlace].zip}
+        />
       </div>
-  );
+    );
+  }
 }
 
-function WeatherDisplay () {
-  return (
-    <h1>Displaying some Weather!</h1>
-  );
+class WeatherDisplay extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json });
+    });
+  }
+  render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    return <div>{JSON.stringify(weatherData)}</div>;
+  }
 }
 
 export default App;
